648d8eb8985795552210bccc278ac22db42a5099
Taliesin <TaliesinYang@163.com>
2022-05-10 10:57:31 +0800

    second commit

--- src/main/java/com/atguigu/test01/demo.java
+++ src/main/java/com/atguigu/test01/demo.java
@@ -3,5 +3,6 @@ package com.atguigu.test01;
 public class demo {
     public static void main(String[] args) {
         System.out.println("git test1");
+        System.out.println("git test2");
     }
 }
---
+++ src/main/java/com/atguigu/test01/demo02.java
@@ -0,0 +1,7 @@
+package com.atguigu.test01;
+
+public class demo02 {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
